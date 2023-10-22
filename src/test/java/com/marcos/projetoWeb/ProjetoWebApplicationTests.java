0a2d8c5e7fd1f4bb85ef79bf448d0553cabe8c7b
MarcosVinicius-Mota <marcosvinicius.nota652790@gmail.com>
2023-10-22 14:05:55 -0300

    initial commit

---
+++ src/test/java/com/marcos/projetoWeb/ProjetoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.projetoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
